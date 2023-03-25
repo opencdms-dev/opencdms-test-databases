f6762fc8f7e6880921a0ed96d84a68540d40b72f
Ian Edwards <isedwards@users.noreply.github.com>
2023-03-25 10:41:15 +0000

    ...

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as readme_file:
 
 setup(
     name='opencdms_test_databases',
-    version="version="0.1.4"",
+    version=0.1.4,
     description='OpenCDMS test databases',
     license="MIT license",
     long_description=readme + "\n",
