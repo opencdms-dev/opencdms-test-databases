c890848b082ca8ba54b19ccb0e7ea31e2b2df425
Ian Edwards <isedwards@users.noreply.github.com>
2023-03-25 10:48:58 +0000

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as readme_file:
 
 setup(
     name='opencdms_test_databases',
-    version="0.1.8",
+    version="0.1.9",
     description='OpenCDMS test databases',
     license="MIT license",
     long_description=readme + "\n",
